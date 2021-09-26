61ab00788c473a19068880f028852d774f9faa67
linneu1997 <linneu1997@gmail.com>
2021-09-26 20:02:44 -0300

    Update readhtml.py

--- readhtml.py
+++ readhtml.py
@@ -1,16 +1,18 @@
-#Classes para a leitura das páginas da web.
-import requests
+#Classes relaconadas a extração de informação de arquivos HTML (parsing).
 from bs4 import BeautifulSoup
+import readhtml
 
-#classe abstrata base para realizar a leitura das URL:
-class read_URL(object):
-	def get_URL():
+#Classe base para extrair informações:
+class extract:
+	def news_title():
 		return
 
-#classe para ler a página da UOL:
-class read_UOL(read_URL):
-	#lê a página a partir da URL:
-	def get_URL(self):
-		url = 'https://www.uol.com.br/'
-		pagina = requests.get(url)
-		self.conteudo = BeautifulSoup(pagina.text, 'html.parser')
+#Classe que extrai informações da página da UOL:
+class extract_UOL:
+	#método que extrai os títulos das notícias:
+	def news_title(self):
+		pagina = readhtml.read_UOL()
+		pagina.get_URL()
+		#as noticias da UOL tem seus titulos pertencentes às classes "titulo color2", com as tags "h1" e "h2".
+		self.lista_de_noticias = pagina.conteudo.find_all("h1", class_="titulo color2")
+		self.lista_de_noticias.extend(pagina.conteudo.find_all("h2", class_="titulo color2"))
