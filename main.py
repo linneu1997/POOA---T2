9b3e76cd2702ca91bfd260144bd8be5a21de9a8b
linneu1997 <linneu1997@gmail.com>
2021-09-26 20:03:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import readhtml
 import extractinfo
 import csv
 
-f = open('dump', 'w')
-writer = csv.writer(f)
-teste = extractinfo.extract_UOL()
-teste.news_title()
-lista_de_titulos = []
-for noticia in teste.lista_de_noticias:
-	titulo = noticia.contents[0]
-	lista_de_titulos.append(titulo)
-writer.writerows([lista_de_titulos])
+print("Deseja imprimir as noticias de qual pagina?")
+escolha = int(input("1 - UOL  2 - outra\n"))
+
+#salva os titulos das notícias da UOL em um arquivo CSV
+if escolha == 1:
+	#Os titulos são salvos no arquivo CSV "news_dump":
+	file = open('news_dump', 'w')
+	writer = csv.writer(file)
+	#cria o objeto "manchetes" com o titulo das notícias do site da UOL:
+	manchetes = extractinfo.extract_UOL()
+	manchetes.news_title()
+	for noticia in manchetes.lista_de_noticias:
+		#mantém apenas o titulo da notícia:
+		titulo = noticia.contents[0]
+		#formata a lista de títulos e a salva em um arquivo CSV
+		writer.writerows([[titulo]])
+
+#demonstração de como o programa pode ser expandido para incluir mais páginas:
+if escolha == 2:
+	print("Novas páginas a serem adicionadas futuramente\n")
